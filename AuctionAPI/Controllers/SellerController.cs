15fc6df20b6b22ecd9aedf4d0a1ec22227d1bb7e
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-26 17:38:52 +0700

    Create SellerController

---
+++ AuctionAPI/Controllers/SellerController.cs
@@ -0,0 +1,47 @@
+﻿using AuctionAPI.Models;
+using AuctionAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        public SellerController(IUserRepository userRepository)
+        {
+            _userRepository= userRepository;
+        }
+        [HttpPost("CreateSeller")]
+        public async Task<IActionResult> CreateSeller(string username,string? email)
+        {
+            User seller = new Seller()
+            {
+                Username = username,
+                Email = email ?? string.Empty,
+                RegisterationDate= DateTime.Now
+            };
+            await _userRepository.AddUser(seller);
+            await _userRepository.Save();
+            return Ok(seller);
+        }
+        [HttpGet("GetSellers")]
+        public async Task<IActionResult> GetSellers()
+        {
+            var sellers = await _userRepository.GetSellers();
+            return Ok(sellers);
+        }
+        [HttpGet("GetRating")]
+        public async Task<IActionResult> GetRating(int sellerId)
+        {
+            if (_userRepository.SellerExist(sellerId))
+            {
+                Seller seller = await _userRepository.GetSellerById(sellerId);
+                return Ok(seller.GetRating()); 
+            }
+            return BadRequest($"Seller with the id of {sellerId} does not exist.");
+        }
+    }
+}
