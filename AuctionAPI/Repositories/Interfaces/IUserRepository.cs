9727468a9f5ce43b1b0bc5858a607671fce834fd
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-26 16:57:36 +0700

    Create UserRepository

---
+++ AuctionAPI/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using AuctionAPI.Models;
+
+namespace AuctionAPI.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetUsers();
+        Task<User> GetById(int id);
+        Task AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(User user);
+        Task Save();
+        bool UserExist(int id);
+    }
+}
---
+++ AuctionAPI/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using AuctionAPI.Data;
+using AuctionAPI.Models;
+using AuctionAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionAPI.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+                _context= context;
+        }
+        public async Task AddUser(User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+
+        public void DeleteUser(User user)
+        {
+            _context.Users.Remove(user);
+        }
+
+        public async Task<User> GetById(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void UpdateUser(User user)
+        {
+            _context.Users.Update(user);
+        }
+
+        public bool UserExist(int id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+            if(user == null) { return false; }
+            return true;
+        }
+    }
+}
