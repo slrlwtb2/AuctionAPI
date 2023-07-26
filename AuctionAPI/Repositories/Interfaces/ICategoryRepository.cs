eae630d508105c1cc36be3984deb8593b2049f77
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-26 15:03:55 +0700

    AddCategoryRepository

---
+++ AuctionAPI/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using AuctionAPI.Models;
+
+namespace AuctionAPI.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetCategories();
+        Task<Category> GetCategoryById(int id);
+        Task AddCategory(Category category);
+        void DeleteCategory(Category category);
+        void UpdateCategory(Category category);
+        void Save();
+    }
+}
