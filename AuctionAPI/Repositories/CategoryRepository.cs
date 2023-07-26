e170f24edacd42c981d0438db72e3aeafc17710d
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-26 15:03:55 +0700

    AddCategoryRepository

---
+++ AuctionAPI/Repositories/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using AuctionAPI.Data;
+using AuctionAPI.Models;
+using AuctionAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionAPI.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context= context;
+        }
+        public async Task AddCategory(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            _context.Categories.Remove(category);
+        }
+
+        public async Task<List<Category>> GetCategories()
+        {
+           return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryById(int id)
+        {
+            return await _context.Categories.FirstAsync(c => c.Id == id);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+        }
+    }
+}
---
+++ AuctionAPI/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using AuctionAPI.Models;
+
+namespace AuctionAPI.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetCategories();
+        Task<Category> GetCategoryById(int id);
+        Task AddCategory(Category category);
+        void DeleteCategory(Category category);
+        void UpdateCategory(Category category);
+    }
+}
