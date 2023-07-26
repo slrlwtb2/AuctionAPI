51c74bff70c835d33ed87fcbe6f10f2a8f98a7b1
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-26 16:57:56 +0700

    FixBalancetoPublic

---
+++ AuctionAPI/Migrations/20230726092556_FixBalanceToPublic.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBalanceToPublic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Balance",
+                table: "Users",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Users");
+        }
+    }
+}
