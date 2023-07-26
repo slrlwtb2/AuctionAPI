7d994e15cc0acfba8a76c93e025639abd0f8d099
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-26 19:34:22 +0700

    Create Product Repository

---
+++ AuctionAPI/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using AuctionAPI.Models;
+
+namespace AuctionAPI.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetProducts();
+        Task<Product> GetProductById(int id);
+        Task AddProduct(Product product);
+        void UpdateProduct(Product product);    
+        void DeleteProduct(Product product);
+        bool ProductExist(int id);
+        Task Save();
+        Task<List<Product>> GetBidableProducts(DateTime dateTime);
+    }
+}
---
+++ AuctionAPI/Repositories/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using AuctionAPI.Data;
+using AuctionAPI.Models;
+using AuctionAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionAPI.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context= context;
+        }
+        public async Task AddProduct(Product product)
+        {
+            await _context.Products.AddAsync(product);
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _context.Products.Remove(product);
+        }
+
+        public async Task<Product> GetProductById(int id)
+        {
+            return await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<Product>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+
+        public bool ProductExist(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            if (product == null) { return false; }
+            return true;
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _context.Products.Update(product);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Product>> GetBidableProducts(DateTime dateTime)
+        {
+          var products =  await _context.Products.Where(p => p.BidEndTime >= dateTime).ToListAsync();
+            return products;
+        }
+    }
+}
