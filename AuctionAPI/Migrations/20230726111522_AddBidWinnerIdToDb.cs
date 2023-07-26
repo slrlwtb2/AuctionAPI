c3e53c55d86a2ba1edb55bc4e94e74481a18cba0
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-26 19:35:22 +0700

    New Migrations

---
+++ AuctionAPI/Migrations/20230726111522_AddBidWinnerIdToDb.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBidWinnerIdToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BidWinnerId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BidWinnerId",
+                table: "Products",
+                column: "BidWinnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_BidWinnerId",
+                table: "Products",
+                column: "BidWinnerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_BidWinnerId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_BidWinnerId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "BidWinnerId",
+                table: "Products");
+        }
+    }
+}
