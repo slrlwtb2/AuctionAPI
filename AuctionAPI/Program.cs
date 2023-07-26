62eeb28581e34e98f5b403785b6bcaeda75c6975
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-26 19:33:50 +0700

    AddScoped for IProductRepository

--- AuctionAPI/Program.cs
+++ AuctionAPI/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
