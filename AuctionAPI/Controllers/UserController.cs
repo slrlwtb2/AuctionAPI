152d426d40e52092f67faf277073bcbf9ccf4402
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-26 17:39:11 +0700

    update usercontroller validationUpdate UserController

--- AuctionAPI/Controllers/UserController.cs
+++ AuctionAPI/Controllers/UserController.cs
@@ -43,18 +43,46 @@ namespace AuctionAPI.Controllers
         [HttpPut("IncreaseBalance")]
         public async Task<IActionResult> IncreaseBalnace(int userId, float amount)
         {
-            User user = await _userRepository.GetById(userId);
-            user.IncreaseBalance(amount);
-            await _userRepository.Save();
-            return Ok($"User:{user.Username}'s balance has increased by {amount}");
+            if (_userRepository.UserExist(userId))
+            {
+                User user = await _userRepository.GetById(userId);
+                user.IncreaseBalance(amount);
+                await _userRepository.Save();
+                return Ok($"User:{user.Username}'s balance has increased by {amount}"); 
+            }
+            return BadRequest($"User with the id of {userId} does not exist.");
         }
         [HttpPut("DecreaseBalance")]
         public async Task<IActionResult> DecreaseBalance(int userId, float amount)
         {
-            User user = await _userRepository.GetById(userId);
-            user.DecreaseBalance(amount);
-            await _userRepository.Save();
-            return Ok($"User:{user.Username}'s balance has decreased by {amount}");
+            if (_userRepository.UserExist(userId))
+            {
+                User user = await _userRepository.GetById(userId);
+                user.DecreaseBalance(amount);
+                await _userRepository.Save();
+                return Ok($"User:{user.Username}'s balance has decreased by {amount}."); 
+            }
+            return BadRequest($"User with the id of {userId} does not exist.");
+        }
+        [HttpPut("GiveRatingToSeller")]
+        public async Task<IActionResult> GiveRatingToSeller(int sellerId, int amount)
+        {
+            if (_userRepository.SellerExist(sellerId))
+            {
+                Seller seller = await _userRepository.GetSellerById(sellerId);
+                if (amount <= 5 && amount >= 0)
+                {
+                    seller.Rating += amount;
+                    seller.RatingCount += 1;
+                }
+                else
+                {
+                    return BadRequest("Rating must be between 0 to 5.");
+                }
+                await _userRepository.Save();
+                return Ok($"{seller.Username}'s rating has increased by {amount}.");
+            }
+            return BadRequest($"Seller with the id of {sellerId} does not exist.");
         }
 
     }
