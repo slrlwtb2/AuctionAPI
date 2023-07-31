4fac90edb81257a23c7c3a2b31dae9b52ab9a608
slrlwtb2 <46591302+slrlwtb2@users.noreply.github.com>
2023-07-31 15:37:41 +0700

    addDTOs

---
+++ AuctionAPI/DTOs/ProductDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionAPI.DTOs
+{
+    public class ProductDTO
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "CategoryId is required.")]
+        public int CategoryId { get; set; }
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string Description { get; set; } = string.Empty;
+        [Range(0.0, double.MaxValue, ErrorMessage = "StartingPrice must be a non-negative value.")]
+        public float StarttingPrice { get; set; }
+        [Required(ErrorMessage = "BidEndTime is required.")]
+        public double BidEndTime { get; set; }
+    }
+}
---
+++ AuctionAPI/DTOs/UpdateProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AuctionAPI.DTOs
+{
+    public class UpdateProductDTO
+    {
+        public string? Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public int? CategoryId { get; set; } = null;
+        public float? StarttingPrice { get; set; } = null;
+
+    }
+}
